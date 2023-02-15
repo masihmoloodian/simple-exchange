71ee64572aeb4b6f76c3dfa718d4510cd45337d9
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-15 17:02:49 +0330

    - feat: Add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    const allow =
+        process.env.NODE_ENV === 'production'
+            ? [process.env.ALLOWED_ORIGIN_DOMAIN_NAME]
+            : ['http://localhost:3001'];
+    app.enableCors({
+        origin: '*',
+        methods: ['GET', 'POST', 'UPDATE', 'PUT', 'DELETE', 'PATCH'],
+        credentials: true,
+        allowedHeaders: ['Authorization', 'Content-Type', 'Set-Cookie'],
+    });
+
     const options = new DocumentBuilder()
         .addBearerAuth()
         .setTitle('RedAcre')
