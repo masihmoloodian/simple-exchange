d675a2dc71839b07b04c6670e2a582b85ac974f0
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-15 18:08:16 +0330

    - feat: Add class validator check data type

--- src/exchange/dto/create-exchange.dto.ts
+++ src/exchange/dto/create-exchange.dto.ts
@@ -1,18 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
 import { CurrentFrom } from '../enum/currency-from.enum';
 import { CurrentTo } from '../enum/currency-to.enum';
 
 export class CreateExchangeDto {
     @ApiProperty({ enum: CurrentFrom })
     @IsNotEmpty()
+    @IsEnum(CurrentFrom)
     currencyFrom: CurrentFrom;
 
     @ApiProperty()
     @IsNotEmpty()
+    @IsNumber()
     amountFrom: number;
 
     @ApiProperty({ enum: CurrentTo })
     @IsNotEmpty()
+    @IsEnum(CurrentTo)
     currencyTo: CurrentTo;
 }
--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -3,16 +3,19 @@ import { ExchangeService } from './exchange.service';
 import { CreateExchangeDto } from './dto/create-exchange.dto';
 import { ListResponseDTO, ResponseDTO } from 'src/shared/dto/response.dto';
 import { GetExchangeDto } from './dto/get-exchange.dto';
+import { ApiOperation } from '@nestjs/swagger';
 @Controller('exchange')
 export class ExchangeController {
     constructor(private readonly exchangeService: ExchangeService) {}
 
     @Post()
+    @ApiOperation({ summary: 'Add new exchange' })
     async create(@Body() dto: CreateExchangeDto) {
         return new ResponseDTO(await this.exchangeService.create(dto));
     }
 
     @Get()
+    @ApiOperation({ summary: 'Get exchange history' })
     async getAll(@Query() dto: GetExchangeDto) {
         const response = await this.exchangeService.getAll(dto);
         return new ListResponseDTO(response[0], response[1]);
