0dc3d5d74e52c91719b8f398900f3e0507056c0b
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-17 09:43:08 +0330

    Add typeorm global config

---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  type: 'mysql',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  synchronize: true,
+  ssl: false,
+  migrations: ['dist/shared/migrations/*.js'],
+  migrationsRun: false,
+  cli: {
+    migrationsDir: 'src/shared/migrations',
+  },
+  logging: false,
+  logger: 'file',
+};
