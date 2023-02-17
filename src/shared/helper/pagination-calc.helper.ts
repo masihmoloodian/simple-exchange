0f0e0d32354b1125a6da0cae3b74e9899e634855
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-17 11:22:06 +0330

    - feat: Add pagination calc

---
+++ src/shared/helper/pagination-calc.helper.ts
@@ -0,0 +1,20 @@
+import { BadRequestException } from '@nestjs/common';
+
+/**
+ *
+ * @param pageNumber
+ * @returns [TAKE, SKIP]
+ */
+export async function paginationCalculator(
+    pageNumber: number
+): Promise<[number, number]> {
+    const TAKE = 12;
+    let SKIP = 0;
+
+    if (pageNumber) {
+        SKIP = (pageNumber - 1) * TAKE;
+        if (SKIP < 0) throw new BadRequestException('invalid page number');
+    }
+
+    return [TAKE, SKIP];
+}
