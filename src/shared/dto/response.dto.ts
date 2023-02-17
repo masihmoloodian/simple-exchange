0d53dfbb10fcc5835a8411f9620f0edda871538b
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-17 11:22:32 +0330

    - feat: Add List Respones DTO

--- src/shared/dto/response.dto.ts
+++ src/shared/dto/response.dto.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { TotalSkipDto } from './total-skip.dto';
 
 export class ResponseDTO {
     constructor(data: any, message: string = 'Success', code: number = 200) {
@@ -12,6 +13,38 @@ export class ResponseDTO {
     @ApiProperty()
     @IsNotEmpty()
     data: any;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    code: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    message: string;
+}
+
+export class ListResponseDTO {
+    constructor(
+        data: any,
+        metadata: TotalSkipDto = { total: null, page: null },
+        message: string = 'Success',
+        code: number = 200
+    ) {
+        this.data = data;
+        this.metadata = metadata;
+        this.message = message;
+        this.code = code;
+    }
+
+    @ApiProperty()
+    @IsNotEmpty()
+    data: any;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    metadata: TotalSkipDto;
 
     @ApiProperty()
     @IsNotEmpty()
