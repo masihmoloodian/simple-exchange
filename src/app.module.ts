66e5f09fd5c552363c27e617d26923617e11bd76
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-17 10:37:54 +0330

    - feat: Create exchange temp module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExchangeModule } from './exchange/exchange.module';
 
 @Module({
     imports: [
@@ -16,6 +17,7 @@ import { AppService } from './app.service';
             synchronize: true,
             ssl: false,
         }),
+        ExchangeModule,
     ],
     controllers: [AppController],
     providers: [AppService],
