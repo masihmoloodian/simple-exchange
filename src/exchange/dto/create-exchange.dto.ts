7c114f6c38ef3b5d8dde24d0c904174f72f69a60
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-17 11:24:43 +0330

    - feat: Add create exchage dto

--- src/exchange/dto/create-exchange.dto.ts
+++ src/exchange/dto/create-exchange.dto.ts
@@ -1,1 +1,18 @@
-export class CreateExchangeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+import { CurrentFrom } from '../enum/currency-from.enum';
+import { CurrentTo } from '../enum/currency-to.enum';
+
+export class CreateExchangeDto {
+    @ApiProperty({ enum: CurrentFrom })
+    @IsNotEmpty()
+    currencyFrom: CurrentFrom;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    amountFrom: string;
+
+    @ApiProperty({ enum: CurrentTo })
+    @IsNotEmpty()
+    currencyTo: CurrentTo;
+}
