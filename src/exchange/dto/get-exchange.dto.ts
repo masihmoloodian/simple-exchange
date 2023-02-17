4a581970c665e86672bb9752bd91bfd54147c998
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-17 11:38:43 +0330

    - feat: Add get exchange dto

---
+++ src/exchange/dto/get-exchange.dto.ts
@@ -0,0 +1,14 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { PaginationDto } from 'src/shared/dto/pagination.dto';
+import { ExchangeType } from '../enum/exchange-type.enum';
+
+export class GetExchangeDto extends PaginationDto {
+    @ApiPropertyOptional({ example: new Date() })
+    fromDate: Date;
+
+    @ApiPropertyOptional({ example: new Date() })
+    toDate: Date;
+
+    @ApiPropertyOptional({ enum: ExchangeType })
+    type: ExchangeType;
+}
