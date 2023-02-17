b8eba49d6d7b165797425fcab317505b1dfb05ee
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-17 10:37:15 +0330

    - feat: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .addBearerAuth()
+        .setTitle('RedAcre')
+        .setDescription('The RedAcre API description')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api/doc', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
