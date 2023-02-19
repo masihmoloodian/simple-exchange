78e70c6c39bb7f1ebfed85ca66bbdaba94d60741
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-19 16:35:02 +0330

    - feat: Add unit test for get exchange

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ExchangeHistoryEntity } from './entities/exchange-history.entity';
+import { CurrencyCrypto } from './enum/currency-from.enum';
+import { CurrencyFiat } from './enum/currency-to.enum';
+import { ExchangeType } from './enum/exchange-type.enum';
 import { ExchangeService } from './exchange.service';
 
 describe('ExchangeService', () => {
-  let service: ExchangeService;
+    let service: ExchangeService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
-    }).compile();
+    const mockExchangeHistory = {
+        save: jest.fn().mockImplementation((ex) =>
+            Promise.resolve({
+                created_at: expect.any(Date),
+                updated_at: expect.any(Date),
+                id: expect.any(String),
+                ...ex,
+            })
+        ),
+    };
 
-    service = module.get<ExchangeService>(ExchangeService);
-  });
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ExchangeService,
+                {
+                    provide: getRepositoryToken(ExchangeHistoryEntity),
+                    useValue: mockExchangeHistory,
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<ExchangeService>(ExchangeService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('should be create a exchange record and return it', async () => {
+        expect(
+            await service.create({
+                currencyFrom: CurrencyCrypto.BTC,
+                amountFrom: 1,
+                currencyTo: CurrencyFiat.USD,
+                amountTo: 25000,
+            })
+        ).toEqual({
+            created_at: expect.any(Date),
+            updated_at: expect.any(Date),
+            id: expect.any(String),
+            currencyFrom: CurrencyCrypto.BTC,
+            amountFrom: '1',
+            currencyTo: CurrencyFiat.USD,
+            amountTo: '25000',
+            type: ExchangeType.EXCHANGED,
+        });
+    });
 });
