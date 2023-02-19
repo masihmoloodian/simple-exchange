c359928c377065efebd403af8fc19ae372b71b48
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-19 12:45:49 +0330

    - feat: Send latest price on socket init connection

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,6 +1,5 @@
 import {
     WebSocketGateway,
-    SubscribeMessage,
     MessageBody,
     OnGatewayConnection,
     OnGatewayDisconnect,
@@ -8,29 +7,55 @@ import {
     WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { Injectable, Logger } from '@nestjs/common';
-import { PriceSocketDto } from './dto/price-socket.dto';
+import {
+    CACHE_MANAGER,
+    Injectable,
+    Logger,
+    Inject,
+    forwardRef,
+} from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { PriceService } from 'src/price/price.service';
 
 @WebSocketGateway({ cors: true })
 @Injectable()
 export class SocketGateway
     implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+    constructor(
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+
+        @Inject(forwardRef(() => PriceService))
+        private readonly priceService: PriceService
+    ) {}
+
     @WebSocketServer() server: Server;
     private logger: Logger = new Logger('SocketGateway');
 
     afterInit() {
         this.logger.log('Init socket');
     }
-    handleConnection(client: Socket) {
+
+    async handleConnection(client: Socket) {
         this.logger.log(`Socket Client connected: ${client.id}`);
+
+        // Send price to client when connected
+        let price = await this.cacheManager.get('price');
+        if (!price) price = await this.priceService.getAllLivePriceWithStyle();
+
+        this.send('price', price);
     }
 
     handleDisconnect(client: Socket) {
         this.logger.log(`Socket Client disconnected: ${client.id}`);
     }
 
-    sendPrice(@MessageBody() body: any) {
-        this.server.emit('price', body);
+    /**
+     * Send payload on socket with given event name
+     * @param event Socket event name
+     * @param body any type
+     */
+    send(event: string, @MessageBody() body: any) {
+        this.server.emit(event, body);
     }
 }
--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, forwardRef, Module } from '@nestjs/common';
+import { PriceModule } from 'src/price/price.module';
 import { SocketGateway } from './socket.gateway';
 
 @Module({
+    imports: [CacheModule.register(), forwardRef(() => PriceModule)],
     providers: [SocketGateway],
     exports: [SocketGateway],
 })
