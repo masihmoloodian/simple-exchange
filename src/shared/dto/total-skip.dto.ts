1919f3102ec9d5c0ba626d5bb28cd95e69bcc248
Masih Moloodian <masihmoloodian@gmail.com>
2023-02-19 12:41:45 +0330

    - feat: Add validator

--- src/shared/dto/total-skip.dto.ts
+++ src/shared/dto/total-skip.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
 export class TotalSkipDto {
     @ApiProperty()
+    @IsNumber()
     total: number;
 
     @ApiProperty()
+    @IsNumber()
     page: number;
 }
